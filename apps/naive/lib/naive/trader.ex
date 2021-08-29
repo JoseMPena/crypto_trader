6c36f9d3f6d88949aaa164524101fac85d3ab111
José M. Peña <josmpena91@gmail.com>
2021-08-29 22:23:10 +0200

    Place a BUY order using a naive strategy

---
+++ apps/naive/lib/naive/trader.ex
@@ -0,0 +1,47 @@
+defmodule Naive.Trader do
+  use GenServer
+  require Logger
+  alias Streamer.Binance.TradeEvent
+
+  defmodule State do
+    @enforce_keys [:symbol, :profit_interval, :tick_size]
+    defstruct [
+      :symbol,
+      :buy_order,
+      :sell_order,
+      :profit_interval,
+      :tick_size
+    ]
+  end
+
+  def start_link(%{} = args) do
+    GenServer.start_link(self(), args, name: :trader)
+  end
+
+  def init(%{symbol: symbol, profit_interval: profit_interval}) do
+    sybmol = String.upcase(symbol)
+    Logger.info("Initializing trader for #{symbol}")
+    tick_size = fetch_tick_size(symbol)
+    {:ok, %State{symbol: symbol, profit_interval: profit_interval, tick_size: tick_size}}
+  end
+
+  def handle_cast(%TradeEvent{price: price}, %State{symbol: symbol, buy_order: nil} = state) do
+    quantity = "100"
+    Logger.info("Placing BUY order for #{symbol} @ #{price}, quantity: #{quantity}")
+
+    {:ok, %Binance.OrderResponse{} = order} =
+      Binance.order_limit_buy(symbol, quantity, price, "GTC")
+
+    {:noreply, %{state | buy_order: order}}
+  end
+
+  defp fetch_tick_size(symbol) do
+    Binance.get_exchange_info()
+    |> elem(1)
+    |> Map.get(:symbols)
+    |> Enum.find(&(&1["symbol"] == symbol))
+    |> Map.get(:filters)
+    |> Enum.find(&(&1["filterType"] == "PRICE_FILTER"))
+    |> Map.get(:tickSize)
+  end
+end
